f3a4b772f6cb52bd3532f0caab28f2694fb6fa02
kewuaa <202321011111@std.uestc.edu.cn>
2024-02-21 22:32:28 +0800

    update keymap for copyurl

--- config.h
+++ config.h
@@ -219,8 +219,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,               XK_l,           copyurl,        {.i =  0} },
-	{ MODKEY|ShiftMask,     XK_L,           copyurl,        {.i =  1} },
+	{ MODKEY,               XK_n,           copyurl,        {.i =  0} },
+	{ MODKEY|ShiftMask,     XK_N,           copyurl,        {.i =  1} },
 	{ MODKEY,               XK_o,           opencopied,     {.v = "xdg-open"} },
 	{ MODKEY,               XK_c,           normalMode,     {.i =  0} },
 };
